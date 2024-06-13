3d81bb676ef1e802139c74762a743bcb146c5663
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-06-13 19:26:28 -0300

    Feat Integration Fetch Api

---
+++ Finance-Front/src/hooks/useFetchApi.tsx
@@ -0,0 +1,42 @@
+import { createContext, ReactNode, useCallback, useState } from "react";
+import { Category } from "../Services/apiTypes";
+import { CreateCategoryData } from "../validators/types";
+import { ApiService } from "../Services/api";
+
+interface FetchAPIProps {
+    createCategory: (data: CreateCategoryData) => Promise<void>;
+    fetchCategories: () => Promise<void>;
+    categories: Category[];
+}
+
+const FetchAPIContext = createContext<FetchAPIProps>({} as FetchAPIProps);
+
+type FetchAPIProviderProps = {
+    children: ReactNode;
+}
+
+export function FetchAPIProvider({ children }: FetchAPIProviderProps) {
+    const [categories, setCategories] = useState<Category[]>([])
+
+    const createCategory = useCallback(async (data: CreateCategoryData) => {
+
+        await ApiService.createCategory(data)
+
+    }, [])
+
+    const fetchCategories = useCallback(async () => {
+
+        const data = await ApiService.getCategory()
+
+        setCategories(data)
+    }, [])
+
+    return (
+        <FetchAPIContext.Provider value= {{ categories, createCategory, fetchCategories }
+}>
+    { children }
+    </FetchAPIContext.Provider>
+    )
+
+
+}
